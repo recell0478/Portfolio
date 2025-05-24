5c5108a71ad7f2b5f7e5983e3124bbf6d1f7dc9c
Rachel Pastreich <rachpast@bu.edu>
2025-05-24 15:13:47 -0400

    added styling for the home page - still have to make it responsive for mobile and smaller screen

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import Image from "next/image";
-
+import profile from "./assets/home.png";
+import styles from "./page.module.css";
 function Home() {
   return (
     <div>
-      <h1>Rachel</h1>
-      <h1>Pastreich</h1>
-      <p>Student at Boston University</p>
-      <p>Bachelor's in Computer Science</p>
-      <p>Kilachand Honors College</p>
-      <Image
-        src="/assets/home/profile.jpg"
-        alt="Rachel Pastreich"
-        width={200}
-        height={200}
-      />
+      <div className={styles.header}>
+        <div className={styles.nameContainer}>
+          <h1 className={styles.name}>Rachel</h1>
+          <h1 className={styles.name2}>Pastreich</h1>
+        </div>
+
+        <div className={styles.profileContainer}>
+          <img
+            src={profile.src}
+            className={styles.profile}
+            alt="Rachel Pastreich"
+          />
+        </div>
+      </div>
+
+      <div className={styles.description}>
+        <p>Student at Boston University</p>
+        <p>Bachelor's in Computer Science</p>
+        <p>Kilachand Honors College</p>
+      </div>
     </div>
   );
 }
